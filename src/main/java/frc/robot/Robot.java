40282ed3da5ef164d0f344ab23ac142fe4a6986f
Gijs Landwehr <gijslandwehr@utexas.edu>
2023-01-19 00:25:39 -0600

    everybud starter code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,119 +4,334 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
+  /*
+   * Autonomous selection options.
+   */
+  private static final String kNothingAuto = "do nothing";
+  private static final String kConeAuto = "cone";
+  private static final String kCubeAuto = "cube";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  // Set up motors, drivetrain, and input
-  private final PWMSparkMax m_leftDrive = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightDrive = new PWMSparkMax(1);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
-  private final XboxController m_controller = new XboxController(0);
+  /*
+   * Drive motor controller instances.
+   * 
+   * Change the id's to match your robot.
+   * Change kBrushed to kBrushless if you are using NEO's.
+   * Use the appropriate other class if you are using different controllers.
+   */
+  CANSparkMax driveLeftSpark = new CANSparkMax(1, MotorType.kBrushed);
+  CANSparkMax driveRightSpark = new CANSparkMax(2, MotorType.kBrushed);
+  VictorSPX driveLeftVictor = new VictorSPX(3);
+  VictorSPX driveRightVictor = new VictorSPX(4);
+
+  /*
+   * Mechanism motor controller instances.
+   * 
+   * Like the drive motors, set the CAN id's to match your robot or use different
+   * motor controller classses (TalonFX, TalonSRX, Spark, VictorSP) to match your
+   * robot.
+   * 
+   * The arm is a NEO on Everybud.
+   * The intake is a NEO 550 on Everybud.
+   */
+  CANSparkMax arm = new CANSparkMax(5, MotorType.kBrushless);
+  CANSparkMax intake = new CANSparkMax(6, MotorType.kBrushless);
 
   /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
+   * The starter code uses the most generic joystick class.
+   * 
+   * The reveal video was filmed using a logitech gamepad set to
+   * directinput mode (switch set to D on the bottom). You may want
+   * to use the XBoxController class with the gamepad set to XInput
+   * mode (switch set to X on the bottom) or a different controller
+   * that you feel is more comfortable.
+   */
+  Joystick j = new Joystick(0);
+
+  /*
+   * Magic numbers. Use these to adjust settings.
+   */
+
+  /**
+   * How many amps the arm motor can use.
+   */
+  static final int ARM_CURRENT_LIMIT_A = 20;
+
+  /**
+   * Percent output to run the arm up/down at
+   */
+  static final double ARM_OUTPUT_POWER = 0.4;
+
+  /**
+   * How many amps the intake can use while picking up
+   */
+  static final int INTAKE_CURRENT_LIMIT_A = 25;
+
+  /**
+   * How many amps the intake can use while holding
+   */
+  static final int INTAKE_HOLD_CURRENT_LIMIT_A = 5;
+
+  /**
+   * Percent output for intaking
+   */
+  static final double INTAKE_OUTPUT_POWER = 1.0;
+
+  /**
+   * Percent output for holding
+   */
+  static final double INTAKE_HOLD_POWER = 0.07;
+
+  /**
+   * Time to extend or retract arm in auto
+   */
+  static final double ARM_EXTEND_TIME_S = 2.0;
+
+  /**
+   * Time to throw game piece in auto
+   */
+  static final double AUTO_THROW_TIME_S = 0.375;
+
+  /**
+   * Time to drive back in auto
+   */
+  static final double AUTO_DRIVE_TIME = 6.0;
+
+  /**
+   * Speed to drive backwards in auto
+   */
+  static final double AUTO_DRIVE_SPEED = -0.25;
+
+  /**
+   * This method is run once when the robot is first started up.
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
+    m_chooser.setDefaultOption("do nothing", kNothingAuto);
+    m_chooser.addOption("cone and mobility", kConeAuto);
+    m_chooser.addOption("cube and mobility", kCubeAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
 
-    // Invert one side of the drivetrain so that positive commands produce forward motion
-    m_rightDrive.setInverted(true);
+    /*
+     * You will need to change some of these from false to true.
+     * 
+     * In the setDriveMotors method, comment out all but 1 of the 4 calls
+     * to the set() methods. Push the joystick forward. Reverse the motor
+     * if it is going the wrong way. Repeat for the other 3 motors.
+     */
+    driveLeftSpark.setInverted(false);
+    driveLeftVictor.setInverted(false);
+    driveRightSpark.setInverted(false);
+    driveRightVictor.setInverted(false);
+
+    /*
+     * Set the arm and intake to brake mode to help hold position.
+     * If either one is reversed, change that here too. Arm out is defined
+     * as positive, arm in is negative.
+     */
+    arm.setInverted(true);
+    arm.setIdleMode(IdleMode.kBrake);
+    arm.setSmartCurrentLimit(ARM_CURRENT_LIMIT_A);
+    intake.setInverted(false);
+    intake.setIdleMode(IdleMode.kBrake);
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
+   * Calculate and set the power to apply to the left and right
+   * drive motors.
+   * 
+   * @param forward Desired forward speed. Positive is forward.
+   * @param turn    Desired turning speed. Positive is counter clockwise from
+   *                above.
+   */
+  public void setDriveMotors(double forward, double turn) {
+    SmartDashboard.putNumber("drive forward power (%)", forward);
+    SmartDashboard.putNumber("drive turn power (%)", turn);
+
+    /*
+     * positive turn = counter clockwise, so the left side goes backwards
+     */
+    double left = forward - turn;
+    double right = forward + turn;
+
+    SmartDashboard.putNumber("drive left power (%)", left);
+    SmartDashboard.putNumber("drive right power (%)", right);
+
+    // see note above in robotInit about commenting these out one by one to set
+    // directions.
+    driveLeftSpark.set(left);
+    driveLeftVictor.set(ControlMode.PercentOutput, left);
+    driveRightSpark.set(right);
+    driveRightVictor.set(ControlMode.PercentOutput, right);
+  }
+
+  /**
+   * Set the arm output power. Positive is out, negative is in.
+   * 
+   * @param percent
+   */
+  public void setArmMotor(double percent) {
+    arm.set(percent);
+    SmartDashboard.putNumber("arm power (%)", percent);
+    SmartDashboard.putNumber("arm motor current (amps)", arm.getOutputCurrent());
+    SmartDashboard.putNumber("arm motor temperature (C)", arm.getMotorTemperature());
+  }
+
+  /**
+   * Set the arm output power.
+   * 
+   * @param percent desired speed
+   * @param amps current limit
+   */
+  public void setIntakeMotor(double percent, int amps) {
+    intake.set(percent);
+    intake.setSmartCurrentLimit(amps);
+    SmartDashboard.putNumber("intake power (%)", percent);
+    SmartDashboard.putNumber("intake motor current (amps)", intake.getOutputCurrent());
+    SmartDashboard.putNumber("intake motor temperature (C)", intake.getMotorTemperature());
+  }
+
+  /**
+   * This method is called every 20 ms, no matter the mode. It runs after
+   * the autonomous and teleop specific period methods.
    */
   @Override
   public void robotPeriodic() {
-    m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getRightX());
+    SmartDashboard.putNumber("Time (seconds)", Timer.getFPGATimestamp());
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
+  double autonomousStartTime;
+  double autonomousIntakePower;
+
   @Override
   public void autonomousInit() {
+    driveLeftSpark.setIdleMode(IdleMode.kBrake);
+    driveLeftVictor.setNeutralMode(NeutralMode.Brake);
+    driveRightSpark.setIdleMode(IdleMode.kBrake);
+    driveRightVictor.setNeutralMode(NeutralMode.Brake);
+
     m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
+
+    if (m_autoSelected == kConeAuto) {
+      autonomousIntakePower = INTAKE_OUTPUT_POWER;
+    } else if (m_autoSelected == kCubeAuto) {
+      autonomousIntakePower = -INTAKE_OUTPUT_POWER;
+    }
+
+    autonomousStartTime = Timer.getFPGATimestamp();
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    if (m_autoSelected == kNothingAuto) {
+      setArmMotor(0.0);
+      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(0.0, 0.0);
+      return;
+    }
+
+    double timeElapsed = Timer.getFPGATimestamp() - autonomousStartTime;
+
+    if (timeElapsed < ARM_EXTEND_TIME_S) {
+      setArmMotor(ARM_OUTPUT_POWER);
+      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(0.0, 0.0);
+    } else if (timeElapsed < ARM_EXTEND_TIME_S + AUTO_THROW_TIME_S) {
+      setArmMotor(0.0);
+      setIntakeMotor(autonomousIntakePower, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(0.0, 0.0);
+    } else if (timeElapsed < ARM_EXTEND_TIME_S + AUTO_THROW_TIME_S + ARM_EXTEND_TIME_S) {
+      setArmMotor(-ARM_OUTPUT_POWER);
+      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(0.0, 0.0);
+    } else if (timeElapsed < ARM_EXTEND_TIME_S + AUTO_THROW_TIME_S + ARM_EXTEND_TIME_S + AUTO_DRIVE_TIME) {
+      setArmMotor(0.0);
+      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(AUTO_DRIVE_SPEED, 0.0);
+    } else {
+      setArmMotor(0.0);
+      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
+      setDriveMotors(0.0, 0.0);
     }
   }
 
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {}
+  /**
+   * Used to remember the last game piece picked up to apply some holding power.
+   */
+  static final int CONE = 1;
+  static final int CUBE = 2;
+  static final int NOTHING = 3;
+  int lastGamePiece;
 
-  /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopInit() {
+    driveLeftSpark.setIdleMode(IdleMode.kCoast);
+    driveLeftVictor.setNeutralMode(NeutralMode.Coast);
+    driveRightSpark.setIdleMode(IdleMode.kCoast);
+    driveRightVictor.setNeutralMode(NeutralMode.Coast);
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
+    lastGamePiece = NOTHING;
+  }
 
-  /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void teleopPeriodic() {
+    double armPower;
+    if (j.getRawButton(7)) {
+      // lower the arm
+      armPower = -ARM_OUTPUT_POWER;
+    } else if (j.getRawButton(5)) {
+      // raise the arm
+      armPower = ARM_OUTPUT_POWER;
+    } else {
+      // do nothing and let it sit where it is
+      armPower = 0.0;
+    }
+    setArmMotor(armPower);
+  
+    double intakePower;
+    int intakeAmps;
+    if (j.getRawButton(8)) {
+      // cube in or cone out
+      intakePower = INTAKE_OUTPUT_POWER;
+      intakeAmps = INTAKE_CURRENT_LIMIT_A;
+      lastGamePiece = CUBE;
+    } else if (j.getRawButton(6)) {
+      // cone in or cube out
+      intakePower = -INTAKE_OUTPUT_POWER;
+      intakeAmps = INTAKE_CURRENT_LIMIT_A;
+      lastGamePiece = CONE;
+    } else if (lastGamePiece == CUBE) {
+      intakePower = INTAKE_HOLD_POWER;
+      intakeAmps = INTAKE_HOLD_CURRENT_LIMIT_A;
+    } else if (lastGamePiece == CONE) {
+      intakePower = -INTAKE_HOLD_POWER;
+      intakeAmps = INTAKE_HOLD_CURRENT_LIMIT_A;
+    } else {
+      intakePower = 0.0;
+      intakeAmps = 0;
+    }
+    setIntakeMotor(intakePower, intakeAmps);
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
+    /*
+     * Negative signs here because the values from the analog sticks are backwards
+     * from what we want. Forward returns a negative when we want it positive.
+     */
+    setDriveMotors(-j.getRawAxis(1), -j.getRawAxis(2));
+  }
 }
